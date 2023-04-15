b34edd400cd6fb43d18d84727bca765f6bc99c31
Danah <danah.kim@kakao.com>
2023-04-15 17:35:04 +0900

    boilerplate Vite w/ vite-plugin-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,9 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import babel from "vite-plugin-babel";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react'
+import ssr from 'vite-plugin-ssr/plugin'
+import { UserConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: "@emotion/react",
-      plugins: [["@swc/plugin-emotion", {}]],
-    }),
-    babel({
-      babelConfig: {
-        presets: [
-          "@babel/preset-react",
-          "@babel/preset-typescript",
-          "@emotion/babel-preset-css-prop",
-        ],
-        plugins: [
-          "@babel/plugin-transform-runtime",
-          "@babel/plugin-proposal-optional-chaining",
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          [
-            "@emotion",
-            {
-              sourceMap: true,
-              autoLabel: "dev-only",
-              labelFormat: "[local]",
-              cssPropOptimization: true,
-            },
-          ],
-        ],
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+const config: UserConfig = {
+  plugins: [react(), ssr()]
+}
+
+export default config
